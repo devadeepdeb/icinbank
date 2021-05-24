e06fef6c714e9296fbf614b0bdf6b3f6eed39661
Devadeep Deb <devadeep.deb@gmail.com>
2021-05-24 21:50:17 +0530

    thirtyoneth commit

--- src/test/java/com/icinbank/ICINBankApplicationTests.java
+++ src/test/java/com/icinbank/ICINBankApplicationTests.java
@@ -1,16 +1,26 @@
 package com.icinbank;
   
- import org.junit.Test; 
- import org.junit.runner.RunWith; 
- import org.springframework.boot.test.context.SpringBootTest; 
+ import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test; 
+ import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest; 
  import org.springframework.test.context.junit4.SpringRunner;
   
   @RunWith(SpringRunner.class)
   @SpringBootTest 
   public class ICINBankApplicationTests {
+	  
+	  public static Logger logger = LoggerFactory.getLogger(ICINBankApplicationTests.class);
   
   @Test 
-  public void contextLoads() { }
+  public void contextLoads() {
+	  
+	  logger.info("Test case executing...");
+	  assertEquals(true, true);
+  }
   
   }
  
