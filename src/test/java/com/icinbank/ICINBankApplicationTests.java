9f5b5edebac70ee995f9187ea133a852d99dd17b
Devadeep Deb <devadeep.deb@gmail.com>
2021-05-28 14:58:42 +0530

    committed

--- src/test/java/com/icinbank/ICINBankApplicationTests.java
+++ src/test/java/com/icinbank/ICINBankApplicationTests.java
@@ -19,7 +19,7 @@ import org.springframework.boot.test.context.SpringBootTest;
   public void contextLoads() {
 	  
 	  logger.info("Test case executing...");
-	  assertEquals(true, SpringApplication.run(ICINBankApplication.class));
+//	  assertEquals(true, SpringApplication.run(ICINBankApplication.class));
 	  ICINsignuptest obj = new ICINsignuptest();
 		obj.Setup();
 		obj.Login();
