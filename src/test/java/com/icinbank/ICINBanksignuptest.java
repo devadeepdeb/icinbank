a1e902399928cf470d67bef7850eca681ea8112a
Devadeep Deb <devadeep.deb@gmail.com>
2021-05-29 14:29:12 +0530

    committed

--- src/test/java/com/icinbank/ICINBanksignuptest.java
+++ src/test/java/com/icinbank/ICINBanksignuptest.java
@@ -57,7 +57,7 @@ public class ICINBanksignuptest {
 			WebElement btnLogin1 = driver.findElement(By.xpath("//button[contains(text(),'Sign up!')]"));
 			btnLogin1.click();
 			
-			WebElement loginLink1 = driver.findElement(By.xpath("//*[@class='form-group ']"));
+			WebElement loginLink1 = driver.findElement(By.xpath("//a[@href='/signup']"));
 			loginLink1.click();
 			
 			WebElement fname1 = driver.findElement(By.name("firstName"));
